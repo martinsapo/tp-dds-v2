ec8143918a41a88ccf988b584ebad7adb11e66b9
msaposnic <martin.saposnic@mercadolibre.com>
2019-01-24 14:55:44 -0300

    arranco con abm de dispositivos

--- src/main/java/com/utn/dds/tpdds/controllers/Cliente/DispositivosABMController.java
+++ src/main/java/com/utn/dds/tpdds/controllers/Cliente/DispositivosABMController.java
@@ -1,10 +1,8 @@
 package com.utn.dds.tpdds.controllers.Cliente;
 
-import com.utn.dds.tpdds.model.ItemDeCatalogoDeDispositivos;
 import com.utn.dds.tpdds.model.ClienteResidencial;
-import com.utn.dds.tpdds.model.Dispositivo;
-import com.utn.dds.tpdds.model.DispositivoEstandar;
 import com.utn.dds.tpdds.model.DispositivoInteligente;
+import com.utn.dds.tpdds.model.ItemDeCatalogoDeDispositivos;
 import com.utn.dds.tpdds.repository.CatalogoDispositivosJpaRepository;
 import com.utn.dds.tpdds.repository.ClienteResidencialJpaRepository;
 import com.utn.dds.tpdds.repository.DispositivoEstandarJpaRepository;
@@ -14,12 +12,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping(value = "cliente/abmDispositivos")
@@ -38,5 +34,27 @@ public class DispositivosABMController {
 
         model.addAttribute("dispositivosDelCatalogo", dispositivosDelCatalogo);
         return new ModelAndView("dispositivosABM", model);
+    }
+
+    @RequestMapping(value = "/alta", method = RequestMethod.GET)
+    public ModelAndView alta(HttpServletRequest request) {
+        ClienteResidencial cliente = ((ClienteResidencial) request.getSession().getAttribute("cliente"));
+        List<DispositivoInteligente> dispositivoInteligentes = cliente.getDispositivosInteligentes();
+        ModelMap model = new ModelMap();
+        //cargar datos para el alta
+        return new ModelAndView("dispositivosAlta", model);
+    }
+
+    @RequestMapping(value = "/alta/submit", method = RequestMethod.GET)
+    public ModelAndView altaSubmit() {
+
+        //guardar dispositivo
+        return new ModelAndView("redirect:/cliente/abmDispositivos");
+    }
+
+    @RequestMapping(value = "/baja/submit", method = RequestMethod.GET)
+    public ModelAndView bajaSubmit(HttpServletRequest request) {
+        //eliminar dispositivo
+        return new ModelAndView("redirect:/cliente/abmDispositivos");
     }
 }
